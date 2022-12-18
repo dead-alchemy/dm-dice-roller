3eb78af5af87fc00125e2c798a28ca4fb00b796b
david <dlonicholas@gmail.com>
2022-12-17 17:30:56 -0700

    host formatting.

--- client/src/pages/host/Host.jsx
+++ client/src/pages/host/Host.jsx
@@ -2,8 +2,19 @@ import {Box, Heading, Button} from "@chakra-ui/react";
 
 const Host = () => {
 	return (
-		<Box>
-			<Heading>Host</Heading>
+		<Box
+			display={"flex"}
+			justifyContent="center"
+			p={2}
+			backgroundColor={"blue.200"}
+			height={"100vh"}
+		>
+			<Box textAlign={"center"}>
+				<Heading>Welcome to Hosting a Game!</Heading>
+				<Box p={2} fontSize={"lg"}>
+					Your Code is:
+				</Box>
+			</Box>
 		</Box>
 	);
 };
